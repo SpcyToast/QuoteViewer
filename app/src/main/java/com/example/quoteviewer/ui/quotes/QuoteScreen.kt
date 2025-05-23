64b9949c9a4b3406909e0e7a060fd44aa0b1e154
Prashniel Kumar <prash.kumar@westpac.co.nz>
2025-05-23 15:06:38 +1200

    replace placeholder quotes

--- app/src/main/java/com/example/quoteviewer/ui/quotes/QuoteScreen.kt
+++ app/src/main/java/com/example/quoteviewer/ui/quotes/QuoteScreen.kt
@@ -1,13 +1,17 @@
 package com.example.quoteviewer.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
@@ -22,6 +26,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -71,12 +78,19 @@ private fun QuoteView(
                         horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
                         Button(
+                            modifier = Modifier.fillMaxHeight().width(200.dp),
+                            shape = RectangleShape,
+                            border = BorderStroke(2.dp, Color(0,0,0)),
                             onClick =
                                 newQuote
                         ) {
                             Text(text = "New Quote")
                         }
+                        Spacer(modifier = Modifier.)
                         Button(
+                            modifier = Modifier.fillMaxSize(),
+                            shape = RectangleShape,
+                            border = BorderStroke(2.dp, Color(0,0,0)),
                             onClick =
                                 viewHistory
                         ) {
@@ -124,10 +138,12 @@ private fun QuoteView(
             Column {
                 screenState.history.forEach { quote ->
                     Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RectangleShape,
+                        border = BorderStroke(2.dp, Color(0,0,0)),
                         onClick = {
                             focusQuote(quote)
                         },
-                        modifier = Modifier.fillMaxWidth(),
                     ) {
                         val thisAuthor = quote.author
                         Column {
--- readme.md
+++ readme.md
@@ -25,8 +25,3 @@ Use Gradle to add necessary dependencies (e.g. for ViewModel, LiveData).
 
 # Bonus (optional):
 - Add a RecyclerView to show a history of viewed quotes.
-
-# Currently Working On:
-- Getting Hilt working 
-- Setting up a random number generator
-- Figure out how to get a history of all quotes
