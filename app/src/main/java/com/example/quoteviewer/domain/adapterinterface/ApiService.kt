33db784db562d79624f87752b847af35d7e3034b
Prashniel Kumar <prash.kumar@westpac.co.nz>
2025-06-03 10:12:56 +1200

    remove ApiService.kt to make branches for restApi methods

--- app/src/main/java/com/example/quoteviewer/domain/adapterinterface/ApiService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.quoteviewer.domain.adapterinterface
-
-//https://medium.com/@humzakhalid94/api-integration-with-jetpack-compose-hilt-viewmodel-retrofit-a-comprehensive-guide-54ef905191bc
-interface ApiService {
-    @GET("quotes")
-    suspend fun getQuotes(): List<Quote>
-}
