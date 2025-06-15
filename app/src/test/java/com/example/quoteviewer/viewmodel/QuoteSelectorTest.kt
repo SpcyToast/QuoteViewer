b63d17aed6a33b985e57a4570625b59f068787f8
Prashniel Kumar <prash.kumar@westpac.co.nz>
2025-06-16 10:26:58 +1200

    fetchQuote test unhappy path

--- app/src/test/java/com/example/quoteviewer/viewmodel/QuoteSelectorTest.kt
+++ app/src/test/java/com/example/quoteviewer/viewmodel/QuoteSelectorTest.kt
@@ -7,6 +7,7 @@ import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class QuoteSelectorTest {
     private val quoteClient = mock<QuoteClient>()
@@ -16,8 +17,23 @@ class QuoteSelectorTest {
 
 
     @Test
-    fun `fetchQuote - get quote from API call - success`()= runTest{
-        whenever(quoteClient.getQuote()).thenReturn(Quote("Test Quote","Test Author","Test Category"))
-        assertEquals(quoteSelector.fetchQuote(),Result.success(Quote("Test Quote","Test Author","Test Category")))
+    fun `fetchQuote - get quote from API call - success`() = runTest {
+        whenever(quoteClient.getQuote()).thenReturn(
+            Quote(
+                "Test Quote",
+                "Test Author",
+                "Test Category"
+            )
+        )
+        assertEquals(
+            quoteSelector.fetchQuote(),
+            Result.success(Quote("Test Quote", "Test Author", "Test Category"))
+        )
+    }
+
+    @Test
+    fun `fetchQuote - get quote from API call - failure`() = runTest {
+        whenever(quoteClient.getQuote()).thenThrow()
+        assertTrue { quoteSelector.fetchQuote().isFailure }
     }
 }
